4438f6d7bb89ba75a698d420f1ad676aad53cadb
Blai Jordan Borobia <blai.jordan@students.salle.url.ediu>
2020-03-03 15:05:36 +0100

    First Commit
    First Commit

---
+++ cmake-build-debug/CMakeFiles/LS_Racing.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/NAS/Blai/UNI/2n/Semestre 2/prog/LS_Racing/LS_allegro.c" "C:/NAS/Blai/UNI/2n/Semestre 2/prog/LS_Racing/cmake-build-debug/CMakeFiles/LS_Racing.dir/LS_allegro.c.obj"
+  "C:/NAS/Blai/UNI/2n/Semestre 2/prog/LS_Racing/main.c" "C:/NAS/Blai/UNI/2n/Semestre 2/prog/LS_Racing/cmake-build-debug/CMakeFiles/LS_Racing.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:||MinGW/include"
+  "C:||MinGW/lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/LS_Racing.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LS_Racing.dir/LS_allegro.c.obj"
+  "CMakeFiles/LS_Racing.dir/main.c.obj"
+  "LS_Racing.exe"
+  "LS_Racing.exe.manifest"
+  "LS_Racing.pdb"
+  "libLS_Racing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LS_Racing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
